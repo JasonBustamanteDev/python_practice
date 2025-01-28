03dfa86b014e26b08cb120f0c4dbe13b77ae83c0
JasonBustamanteDev <jasonxdev@protonmail.com>
2025-01-27 21:00:09 -0500

    Reverse linked list

--- linked_lists/reverse_linked_list.py
+++ linked_lists/reverse_linked_list.py
@@ -1,1 +1,17 @@
-# https://leetcode.com/problems/reverse-linked-list/description/
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head):
+        curr_node, prev_node = head, None
+
+        while curr_node is not None:
+            old_next = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = old_next
+
+        return prev_node
